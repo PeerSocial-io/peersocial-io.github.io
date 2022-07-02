5a48af370b1cabddcef2ca224696629822db2667
Bradley Matusiak <bmatusiak@gmail.com>
2022-07-02 01:09:18 +0000

    update build.sh

--- build.sh
+++ build.sh
@@ -2,4 +2,32 @@
 
 echo "Build Starting"
 
+rm -rf ./node_modules
+rm -rf ./peersocial.io
+
+git clone https://github.com/PeerSocial-io/PeerSocial.io.git ./peersocial.io
+
+cd ./peersocial.io
+npm install
+
+cd ../
+mv ./peersocial.io/node_modules ./node_modules
+cd ./node_modules
+ln -s ../peersocial.io ./peersocial.io
+cd ../peersocial.io
+
+ln -s ../node_modules ./node_modules
+
+npm run build-dev
+
+cp -a ./build ../docs
+
+cd ..
+
+rm -rf ./node_modules
+rm -rf ./peersocial.io
+
+
+
 echo "Build Done"
+
